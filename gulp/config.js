c765d52436296a1f8fb9453e65df5f5e0f2c3bc7
田村 亮弥 <tamuraryoya@team-lab.com>
2017-09-13 19:08:11 +0900

    設定を書き加えた

--- gulp/config.js
+++ gulp/config.js
@@ -2,17 +2,30 @@ const src = './src/';
 const dist = './docs/';
 
 module.exports = {
+  tasks: [
+    'server',
+    'pug',
+    'webpack',
+    'stylus',
+    'watch',
+  ],
+  
   server: {
     port: 43000,
     notify: false,
   },
-  
+
   src: {
+    root: src,
+    html: src,
     js: `${src}javascripts/`,
+    css: `${src}stylesheets/`,
   },
 
   dist: {
     root: dist,
+    html, dist,
     js: `${dist}javascripts/`,
+    src: `${dist}stylehseets/`,
   }
 }
